3fe8129f3d95f3f10eecd30160c7594037e80d90
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-02-11 14:12:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+// move the image
+
+var img = document.getElementById('img');
+img.onClick = function(){
+  img.style.marginLeft = '100px';  
+};
