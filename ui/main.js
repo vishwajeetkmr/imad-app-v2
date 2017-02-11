cd7f29223fe70a5665750055070536b7bcbd9538
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-02-11 14:13:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML = 'New Value';
 
 // move the image
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onClick = function(){
   img.style.marginLeft = '100px';  
 };
