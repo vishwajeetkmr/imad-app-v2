5f07752a98acb4d43cc68ccbf39b364d10ad3161
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 22:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user : 'vishwajeetkmr',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomSecretValue',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
+}));
 
 function createTemplate (data) {
 var title = data.title;
@@ -101,7 +106,12 @@ app.post('/login',function(req, res){
                var salt = dbString.split('$')[2];
                var hashedPassword = hash(password, salt);
                if (hashedPassword === dbString){
+                   
+                   // set the session
+                   req.session.auth = {userId: result.rows[0].id};
+                   
                res.send('credentials correct');
+               
            }else {
                res.send(403).send('username/password is invalid');
            }
