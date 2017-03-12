f2dff270151e85ffcb5fb3540e3afdb7f72feedd
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 20:43:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user : 'vishwajeetkmr',
@@ -53,6 +54,16 @@ return htmlTemplate;
 
 app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input){
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req, res){
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
 var pool = new Pool(config);
