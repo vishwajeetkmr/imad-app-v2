0e0948a944e3992e1642b86a862c1985acb6728d
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 17:21:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/submit-name', function(req, res){
 app.get('/articles/:articleName', function (req, res) {
     // var articleName = req.params.articleName;
     
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName], function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }else{
