6327dfc994cfeaf6b67ca44435803ba43d8a9d9c
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 22:46:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,8 @@ submit.onclick = function(){
      var password = document.getElementById('password').value;
      console.log(username);
      console.log(password);
-     request.open('POST', 'http://vishwajeetkmr.imad.hasura.io/login', true);
-     request.setRequestHeader('content-type', 'application/json');
+     request.open('POST', 'http://vishwajeetkmr.imad.hasura-app.io/login', true);
+     request.setRequestHeader('Content-Type', 'application/json');
      request.send(JSON.stringify({username: username, password: password}));
      
 };
