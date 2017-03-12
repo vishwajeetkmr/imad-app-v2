af2de5cb9619c449b22c72bec8801b8f066632e9
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 11:02:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user : 'vishwajeetkmr',
+    database : 'vishwajeetkmr',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -82,6 +90,13 @@ app.get('/',function(req, res) {
 app.get('/test-db', function(req, res){
    // make a select request
    // return a response with the results
+   pool.query('SELECT * FROM test', function(err, result){
+       if (err){
+           res.status(500).send(err.toString());
+       }else{
+           res.send(JSON.stringify(result));
+       }
+   });
 });
 
 var counter = 0;
