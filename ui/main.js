f77d8877de4313342dee0681e924b2fac21bad4b
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 22:34:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,47 +1,39 @@
-// counter code
-var button = document.getElementById('counter');
 
-button.onclick = function (){
-    
-    // create request to counter
-    var request = new XMLHttpRequest();
-    
-    
-    //capture the request and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-                
-            }
-        }
-        
-    };
-    
-    // make the request
-    request.open('GET','http://vishwajeetkmr.imad.hasura-app.io/counter');
-    request.send(null);
-
-};
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-  //make arequest to the server and send the name
-  //capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i=0; i<names.length; i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+    
+   // create request object
+   var request = new XMLHttprequest();
+   
+   // capture the response and store it in a variable
+   
+   request.onreadystatechange = function () {
+     if(req.readyState === XMLHttprequest.DONE){
+         // Take some action
+         if(request.status === 200){
+             alert('Logged in Successfully');
+         }else if(request.status === 403){
+             alert('Username/password is incorrect');
+         }else if(request.status === 500){
+             alert('Something went wrong on the server');
+        }
+        
+     }
+         
+     
+     };
+     
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+     request.open('POST', 'http://vishwajeetkmr.imad.hasura.io/login', true);
+     request.setRequestHeader('content-type', 'application/json');
+     request.send(JSON.stringify({username: username, password: password}));
+     
 };
-
 
 
 
