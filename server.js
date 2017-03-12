603d643a8692cd9f56ba6453114477a1ab2e6ebe
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-12 20:45:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/',function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash (input){
+function hash (input, salt){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
